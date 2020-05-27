48effc316cb097be9cda231667695a277665c218
unknown <fangkd@umich.edu>
2020-05-27 17:23:00 -0400

    include IMU bias

--- @RI_EKF/RI_EKF.m
+++ @RI_EKF/RI_EKF.m
@@ -8,6 +8,7 @@ classdef RI_EKF < handle
         g;           % gravity vector;
         vk;          % measurement noise;
         Q;           % IMU noise;
+        bias;        % IMU bias;
     end
    
     methods
@@ -17,41 +18,52 @@ classdef RI_EKF < handle
             %      0, 1, 0;
             %      0, 0, 1]
             obj.mu = eye(5);
-            obj.Sigma = 0.1*eye(9);
+            obj.Sigma = 0.1*eye(15);
             obj.delta_t = 1;
             obj.g = [0; 0; -9.81];
-            obj.vk = eye(5);
-            obj.Q  = 100*eye(9);
+            obj.vk = blkdiag(eye(3), zeros(2));
+            obj.Q  = 100*eye(15);
+            obj.bias = zeros(6, 1);
         end
         
         function prediction(obj, u)
             % input: 6-vector: [omega; a].
             omega = u(1:3);
             a = u(4:6);
-            w = obj.euler2SO3(omega);
+            w = obj.euler2so3(omega);
+            
+            % Bias.
+            w = w - obj.bias(1:3);
+            a = a - obj.bias(4:6);
+            w = obj.vec2skew(w);
             
             % Update state with IMU model.
             R = obj.mu(1:3, 1:3);
             v = obj.mu(1:3, 4);
             p = obj.mu(1:3, 5);
             AdX = obj.Adj(obj.mu);
-            obj.mu(1:3, 1:3) = R*obj.Gamma(0, logm(w)*obj.delta_t);
-            obj.mu(1:3, 4) = v + R*obj.Gamma(1, logm(w)*obj.delta_t)*a + obj.g*obj.delta_t;
-            obj.mu(1:3, 5) = p + v*obj.delta_t + R*obj.Gamma(2, logm(w)*obj.delta_t)*a*obj.delta_t^2 + 0.5*obj.g*obj.delta_t^2;        
+            obj.mu(1:3, 1:3) = R*obj.Gamma(0, w*obj.delta_t);
+            obj.mu(1:3, 4) = v + R*obj.Gamma(1, w*obj.delta_t)*a + obj.g*obj.delta_t;
+            obj.mu(1:3, 5) = p + v*obj.delta_t + R*obj.Gamma(2, w*obj.delta_t)*a*obj.delta_t^2 + 0.5*obj.g*obj.delta_t^2;        
         
             % Covariance propagation.
-            A_r = zeros(9, 9);
+            A_r = zeros(15, 15);
             A_r(4:6, 1:3) = obj.vec2skew(obj.g);
             A_r(7:9, 4:6) = eye(3);
+            A_r(1:3, 10:12) = -R;
+            A_r(4:6, 10:12) = -obj.vec2skew(v)*R;
+            A_r(4:6, 13:15) = -R;
+            A_r(7:9, 10:12) = -obj.vec2skew(p)*R;
             % Phi: transition matrix.
             Phi = expm(A_r);
+            AdX = blkdiag(AdX, eye(6));
             obj.Sigma = Phi*obj.Sigma*Phi' + AdX*obj.Q*AdX';
         end
         
         function propagation(obj, y)
             % input: y = [x, y, z], GPS information;
             y = [y; 0; 1];
-            H = [zeros(3, 3), zeros(3, 3), eye(3)];
+            H = [zeros(3), zeros(3), eye(3), zeros(3), zeros(3)];
             b = [zeros(4, 1); 1];
             N = obj.mu * obj.vk * obj.mu';
             S = H*obj.Sigma*H' + N(1:3, 1:3);
@@ -60,12 +72,15 @@ classdef RI_EKF < handle
             r = obj.mu*y - b;
             r = r(1:3);
             r = L * r;
-            obj.mu = expm(obj.stateMat(r))*obj.mu;
-            obj.Sigma = (eye(9)-L*H)*obj.Sigma*(eye(9)-L*H)' + L*N(1:3, 1:3)*L';
+            [state, b] = obj.stateMat(r);
+            obj.bias = obj.bias + b;
+            obj.mu = expm(state)*obj.mu;
+            obj.Sigma = (eye(15)-L*H)*obj.Sigma*(eye(15)-L*H)' + L*N(1:3, 1:3)*L';
         end
         
-        function state = stateMat(obj, vec)
-            % input: vec = [9x1].
+        function [state, bias] = stateMat(obj, vec)
+            % input: vec = [15x1].
+            % including rotation, velocity, position, IMU bias.
             twistR = vec(1:3);
             v = vec(4:6);
             p = vec(7:9);
@@ -73,9 +88,10 @@ classdef RI_EKF < handle
             state(1:3, 1:3) = obj.vec2skew(twistR);
             state(1:3, 4) = v;
             state(1:3, 5) = p;
+            bias = vec(10:15);
         end
         
-        function R = euler2SO3(obj, w)
+        function R = euler2so3(obj, w)
             % input: w = [roll, pitch, yaw]
             rx = w(1);
             ry = w(2);
@@ -84,6 +100,7 @@ classdef RI_EKF < handle
             Ry = [cos(ry) 0 sin(ry); 0 1 0; -sin(ry) 0 cos(ry)]; % base => nav  (level oxts => rotated oxts)
             Rz = [cos(rz) -sin(rz) 0; sin(rz) cos(rz) 0; 0 0 1];
             R  = Rz*Ry*Rx;
+            R = obj.skew2vec(logm(R));
         end
        
         function val_g = Gamma(obj, m, twist)
